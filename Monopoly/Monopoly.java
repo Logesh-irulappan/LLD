c87a6d12a237c49a2e59babacbb90db5f7ef1920
Logesh <146101687+Logesh-irulappan@users.noreply.github.com>
2025-03-20 09:49:46 +0530

    Add files via upload

---
+++ Monopoly/Monopoly.java
@@ -0,0 +1,15 @@
+package Monopoly;
+
+public class Monopoly {
+
+    private Place[] board;
+
+    public Monopoly(int size) {
+        board = new Place[size];
+    }
+
+    public void movePlayer(int steps, Player player) {
+
+    }
+
+}
